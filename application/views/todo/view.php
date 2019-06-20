d39c9fe6f8060df6a1a7576fbe076f0ba6d05cc6
Sanam Shrestha <shrestha722@gmail.com>
2019-06-20 11:27:27 +0545

    View todo completed task

--- application/views/todo/view.php
+++ application/views/todo/view.php
@@ -1,13 +1,17 @@
-<h2 class="text"><?= $title; ?></h2>
-<ul class="list-group">
+<div class="card card-4">
+    <div class="card-body">
+        <h2 class="text"><?= $title; ?></h2>
+            <ul class="list-group">
 
-<?php foreach($todos as $todo): ?>
-	<li class="list-group-item todo"><?php echo $todo['todoname']; ?></a>
+                <?php foreach($todos as $todo): ?>
+                    <li class="list-group-item todo"><?php echo $todo['todoname']; ?></a>
 
-    </li>
-<?php endforeach; ?>
-</ul>
-
-<div class="background">
-
+                    </li>
+                <?php endforeach; ?>
+                </ul>
+    </div>
 </div>
+
+    <div class="background">
+
+    </div>
