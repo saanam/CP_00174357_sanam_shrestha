faa6821fcd4e44e70cfaf077bc87ddee5677dfdf
Sanam Shrestha <shrestha722@gmail.com>
2019-06-21 14:40:13 +0545

    touch on navbar

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -58,11 +58,11 @@
             <!-- checks in session data for displaying these part-->
             <!-- start of if condition-->
             <?php if($this->session->userdata('logged_in')) : ?>
-                    <a class="nav-link" href="<?php echo base_url(); ?>todo">Todo<span class="sr-only">(current)</span></a> 
-                    <a class="nav-link" href="<?php echo base_url(); ?>notes">Notes<span class="sr-only">(current)</span></a> 
+                    <a class="nav-link" href="<?php echo base_url(); ?>todo">Todo<span class="sr-only">(current)</span></a>
                     <a class="nav-link" href="<?php echo base_url(); ?>notebooks">Notebooks<span class="sr-only">(current)</span></a> 
-                    <a class="nav-link" href="<?php echo base_url(); ?>notes/add">Add Note<span class="sr-only">(current)</span></a> 
-                    <a class="nav-link" href="<?php echo base_url(); ?>notebooks/add">Add Notebook<span class="sr-only">(current)</span></a> 
+                    <a class="nav-link" href="<?php echo base_url(); ?>notes">Notes<span class="sr-only">(current)</span></a> 
+                    <a class="nav-link" href="<?php echo base_url(); ?>notebooks/add">Add Notebook<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="<?php echo base_url(); ?>notes/add">Add Note<span class="sr-only">(current)</span></a>                    
                     <a class="nav-link" href="<?php echo base_url(); ?>profile">Profile<span class="sr-only">(current)</span></a>
                     <a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout<span class="sr-only">(current)</span></a>
                       <form class="form-inline my-2 my-lg-0" action="<?php echo base_url(); ?>notes/note_search">
--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -1,7 +1,7 @@
 <div class="card card-4">
     <div class="card-body">
-        <h2 class="text"><?= $title; ?></h2>
-            <span style="color: #0086b3"> Welcome <?php echo $this->session->userdata('email') ;?></span></header><br /><br />
+        <h2 class="text"><center><?= $title; ?></h2>
+            <span style="color: #0086b3"> Welcome <?php echo $this->session->userdata('email') ;?></center></span></header><br /><br />
             <div class="row">
                 <div class="col-sm-3 col-md-3"><center>
                     <a class="btn btn-primary" href="<?php echo base_url(); ?>users/edit_profile">Edit profile</a><br /><br />
@@ -10,8 +10,8 @@
                     <a class="btn btn-primary" href="<?php echo base_url(); ?>export_csv/index">Export data</a><br /><br />
                     </center>
                 </div>
-
-                            <div class="col-sm-4 col-md-4 text-danger">
+                
+                            <div class="profile col-sm-4 col-md-4 text-danger">
                                 <tr>
                                     <th class="text"><center><strong><u>Total number of notes stored:</u></strong><center></th><br /> <br />
                                 </tr>
@@ -27,6 +27,7 @@
                                     }
                                 ?>
                             </div>
+
                                 <div class="col-sm-4 col-md-4 text-danger">
                                 <tr>
                                     <th class="text"><center><strong><u>Total number of tasks completed:</u></strong><center></th><br /> <br />
@@ -39,7 +40,7 @@
                                         </tr>
                                     <?php }
                                     else{
-                                        echo 'You have not completed any tasks. Visit todo and complete your remaning tasks. ';
+                                        echo 'You have not completed any tasks. Visit todo, add task and complete your remaning tasks. ';
                                     }
                                 ?>                                    
                                 </div>
